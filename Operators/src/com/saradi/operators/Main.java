d4fb806114ce609ccf71da0986451135cd9d612d
Yaachaka <rosaarjuna@gmail.com>
2022-10-24 21:09:34 +0530

    Operations

---
+++ Operators/src/com/saradi/operators/Main.java
@@ -0,0 +1,80 @@
+package com.saradi.operators;
+
+public class Main {
+    public static void main(String[] arg)
+    {
+//        int a = 5;
+//        a = a+2;
+//        a += 2;
+//        a++;
+//        a--;
+//        a -= 2;
+//        a /= 2;
+
+        //Comparison Operators
+
+//        int a = 5;
+//        int b = 3;
+
+//        boolean answer = a > b;
+//        boolean answer = a < b;
+//        boolean answer = a == b;
+//        boolean answer = a >= b;
+//        boolean answer = a <= b;
+//        boolean answer = a != b;
+//        System.out.println(answer);
+
+
+        //Logical operators
+        int a = 5;
+        int b = 3;
+
+//        boolean answer = a==5 || b == 2;
+//        boolean answer = a==5 && b == 2;
+//        System.out.println(answer);
+
+        //Conditional Statements
+
+//        if(a > 3)
+//        {
+//            System.out.println("a is greater than 3.");
+//
+//        }
+//        else
+//        {
+//            System.out.println("a is lesser than 3.");
+//        }
+
+//        if (a > 0)
+//        {
+//            System.out.println("a is positive.");
+//        }
+//        else if (a < 0)
+//        {
+//            System.out.println("a is negative.");
+//        }
+//        else
+//        {
+//            System.out.println("a is zero.");
+//        }
+
+
+        switch(a)
+        {
+            case 1:
+                System.out.println("a is one.");
+                break;
+            case 2:
+                System.out.println("a is two.");
+                break;
+            case 3:
+                System.out.println("a is three.");
+                break;
+            default:
+                System.out.println("A is not One, Two or Three.");
+                break;
+
+        }
+        System.out.println("Continue...");
+    }
+}
