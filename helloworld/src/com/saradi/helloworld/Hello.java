a7fe44a2c340552236c4938846ca943a45857047
Yaachaka <rosaarjuna@gmail.com>
2022-10-24 21:07:25 +0530

    Hello World project.

---
+++ helloworld/src/com/saradi/helloworld/Hello.java
@@ -0,0 +1,46 @@
+package com.saradi.helloworld;
+
+public class Hello {
+    public static void main(String[] args)
+    {
+//        System.out.print("This should be standalone sentence.");
+//        System.out.println("Hello World!!!");
+//        System.out.print("This should print in the next line.");
+//
+//        int number = -5;
+//        System.out.println(number);
+//
+//        long myLong = 5;
+//        System.out.println(myLong);
+//
+//        double myDouble = 4.5;
+//        System.out.println(myDouble);
+//
+//        float myFloat = (float) 4.5;
+//        System.out.println(myFloat);
+//
+//        char myChar = 'M';
+//        System.out.println(myChar);
+//
+//        char myChar2 = '\u00AE';
+//        System.out.println(myChar2);
+//
+//        String myString = "Anadi Kaala";
+//        System.out.println(myString);
+//
+//        boolean myBoolean = true;
+//        System.out.println(myBoolean);
+        int a = 6;
+        int b = 2;
+
+        int answer = a % b;
+        System.out.println(answer);
+
+
+        String firstName = "Emma";
+        String lastName = "Watson";
+
+        String fullName = firstName +" "+ lastName;
+        System.out.println(fullName);
+    }
+}
