d966889632cad850cd4152e1d93f375311b045fe
Yaachaka <rosaarjuna@gmail.com>
2022-10-25 11:56:28 +0530

    OOPSection3: Composition and Constants.

---
+++ oopsection3/src/com/saradi/oopsection3/Main.java
@@ -0,0 +1,27 @@
+package com.saradi.oopsection3;
+
+public class Main {
+    public static void main(String[] args) {
+//        Engine engine = new Engine("Renault", 8000);
+//        Car mercedes = new Car("Mercedes AMZ", 2, "Silver", engine);
+//        Car mercedes = new Car("Mercedes AMZ", 2, "Silver", new Engine("Renault", 8000));
+//        System.out.println(mercedes.getName());
+//        System.out.println("Engine Model: "+mercedes.getEngine().getModel());
+
+//        Car mercedes = null;
+//        if(mercedes != null) {
+//            mercedes.getName();
+//        }
+//        else
+//        {
+//            System.out.println("The car was null.");
+//        }
+
+//        final int a = 5;
+//        a = a + 1;
+
+        final Engine engine = new Engine("Renault", 8000);
+        //engine = new Engine("Renault", 7000); //Won't work.
+
+    }
+}
